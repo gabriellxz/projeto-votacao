cd841f44eda767a66cac68664be9fba332a79471
Gabriel <gabrielsilvaoliveira521@gmail.com>
2023-11-14 09:20:24 -0300

    project

--- project/src/componentsSVG/chevron-right/chevron-right.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChevronRight() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M9 18L15 12L9 6" stroke="#F2F2F2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-    )
-}
--- project/src/componentsSVG/navbar-icon/navbar-icon.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="41" height="36" viewBox="0 0 41 36" fill="none">
-            <path d="M3 33H38M3 18H38M3 3H38" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-    )
-}
