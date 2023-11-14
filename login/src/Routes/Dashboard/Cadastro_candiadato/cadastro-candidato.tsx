294badbf54565190de7052ac2ecef174e16f3d6a
Gabriel <gabrielsilvaoliveira521@gmail.com>
2023-11-14 08:59:19 -0300

    form candidato

--- login/src/Routes/Dashboard/Cadastro_candiadato/cadastro-candidato.tsx
+++ login/src/Routes/Dashboard/Cadastro_candiadato/cadastro-candidato.tsx
@@ -1,110 +1,112 @@
+import './style.css'
 import { useState } from "react"
 import api from "../../../config/apiConfig"
 import axios from "axios"
 
-interface viacep {
-    localidade: string;
-    uf: string;
+interface Endereco {
+    cep?: string;
+    logradouro?: string;
+    bairro?: string;
+    localidade?: string;
+    uf?: string;
 }
 
 export default function CadastroCandidato() {
 
+    const [endereco, SetEndereco] = useState<Endereco>({})
     const [valueInput, setValueInput] = useState({
         name: "",
         apelido: "",
         Partido: "",
         cep: "",
-        localidade: "",
-        uf: ""
+        cidade: "",
+        estado: ""
     })
-
-    const [endereco, setEndereco] = useState<viacep>()
     const [cidade, setCidade] = useState("")
     const [estado, setEstado] = useState("")
-
-    const [file, setFile] = useState(null)
-
-    function handleFile(e: any) {
-        setFile(e.target.files[0])
-    }
+    const [imagem, setImagem] = useState<File | null>(null)
 
     function handleValueInput(e: any) {
         setValueInput({ ...valueInput, [e.target.name]: e.target.value })
     }
 
-    const headers = {
-        "headers": {
-            "Content-Type": "multipart/form-data",
+    async function getCep(e: any) {
+        e.preventDefault()
+
+        await axios.get<Endereco>(`https://viacep.com.br/ws/${valueInput.cep}/json/`)
+            .then((response) => {
+                console.log(response.data)
+                SetEndereco(response.data)
+                preencherCampos(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    function preencherCampos(endereco: Endereco) {
+        setCidade(endereco.localidade || "")
+        setEstado(endereco.uf || "")
+    }
+
+    function handleImagem(e: any) {
+        if (e.target.files && e.target.files.length > 0) {
+            setImagem(e.target.files[0])
         }
     }
 
-    async function handleSubmitForm(e: any) {
+    const headers = {
+        "Content-Type": "multipart/form-data"
+    }
+
+    async function handlePostForm(e: any) {
         e.preventDefault()
 
-        const formData = new FormData()
+        const formData = new FormData();
         formData.append("name", valueInput.name)
         formData.append("apelido", valueInput.apelido)
         formData.append("Partido", valueInput.Partido)
-        formData.append("localidade", valueInput.localidade)
-        formData.append("uf", valueInput.uf)
-
-        if (file) {
-            formData.append("images", file)
+        formData.append("cep", valueInput.cep)
+        formData.append("cidade", cidade)
+        formData.append("estado", cidade)
+        if (imagem) {
+            formData.append("imagem", imagem)
         }
 
-        // await api.post("/Candidatos", formData, headers)
-        //     .then((response) => {
-        //        setValueInput(response.data)
-        //        console.log(response.data)
-        //     })
-        //    .catch((err) => {
-        //      console.log(err)
-        //    })
-
-        await api.post("https://api-sistema-de-votacao.vercel.app/Candidatos", formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        })
+        await api.post("/Candidatos", formData, { headers })
             .then((response) => {
                 console.log(response.data)
             })
             .catch((err) => {
                 console.log(err)
             })
-
-    }
-
-    async function getCep() {
-        await axios.get(`https://viacep.com.br/ws/${valueInput.cep}/json/`)
-            .then((response) => {
-                console.log(response.data)
-                preencherEndereco(response.data)
-                setEndereco(response.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
-
-    function preencherEndereco(endereco: viacep) {
-        setCidade(endereco.localidade)
-        setEstado(endereco.uf)
     }
 
     return (
         <>
             <div className="container-cadastro-candidato">
-                <form onSubmit={handleSubmitForm}>
-                    <input type="text" name="name" placeholder="Nome" onChange={handleValueInput} />
-                    <input type="text" name="apelido" placeholder="Apelido" onChange={handleValueInput} />
-                    <input type="text" name="Partido" placeholder="Partido" onChange={handleValueInput} />
-                    <input type="text" name="cep" placeholder="CEP" onChange={handleValueInput} />
-                    <button onClick={getCep}>Buscar CEP</button>
-                    <input type="text" name="localidade" placeholder="Cidade" onChange={handleValueInput} value={cidade} />
-                    <input type="text" name="uf" placeholder="Estado" onChange={handleValueInput} value={estado} />
-                    <input type="file" onChange={handleFile} />
-                    <button>Cadastrar</button>
+                <form>
+                    <div className="single-input">
+                        <input type="text" name="name" placeholder="Nome" onChange={handleValueInput} />
+                        <input type="text" name="apelido" placeholder="Apelido" onChange={handleValueInput} />
+                    </div>
+                    <div className="single-input">
+                        <input type="text" name="Partido" placeholder="Partido" onChange={handleValueInput} />
+                    </div>
+                    <div className="single-input">
+                        <input type="text" name="cep" placeholder="CEP" onChange={handleValueInput} />
+                        <button onClick={getCep}>Buscar CEP</button>
+                    </div>
+                    <div className="single-input">
+                        <input type="text" name="cidade" placeholder="Cidade" value={cidade} />
+                        <input type="text" name="estado" placeholder="Estado" value={estado} />
+                    </div>
+                    <div className="single-input">
+                        <input type="file" onChange={handleImagem} />
+                    </div>
+                    <div className="single-input">
+                        <button onClick={handlePostForm}>Cadastrar</button>
+                    </div>
                 </form>
             </div>
         </>
--- login/src/config/apiConfig.ts
+++ login/src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "https://api-sistema-de-votacao.vercel.app"
+    baseURL: "http://localhost:3000"
 })
