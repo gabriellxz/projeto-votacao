2f40f55183e1b25017ade70a910d4dd14361bed6
Gabriel Silva <114831887+gabriellxz@users.noreply.github.com>
2024-05-01 15:41:08 -0300

    Update login.tsx

--- front-end/src/pages/Login/login.tsx
+++ front-end/src/pages/Login/login.tsx
@@ -99,11 +99,13 @@ export default function Login() {
                     </div>
                     <div className="container-title">
                         <h1>Fazer login</h1>
-                        <div className="msg-error">
-                            {mensagem.type === "error" ? <p>{mensagem.mensagem}</p> : ""}
-                        </div>
-                        <div className="msg-sucess">
-                            {mensagem.loading ? <LoadingIcon /> : ""}
+                        <div>
+                            <div className="msg-error">
+                                {mensagem.type === "error" ? <p>{mensagem.mensagem}</p> : ""}
+                            </div>
+                            <div className="msg-sucess">
+                                {mensagem.loading ? <LoadingIcon /> : ""}
+                            </div>
                         </div>
                     </div>
                     <form className="container-form" onSubmit={handlePostForm}>
